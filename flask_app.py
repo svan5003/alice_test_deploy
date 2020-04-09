b4b1ce05aeeb4084ffac7037b43f725af9b45356
svan5003 <svan5003@gmail.com>
2020-04-09 12:19:10 +0300

    first commit

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 # импортируем библиотеки
 from flask import Flask, request
 import logging
+import os
 
 # библиотека, которая нам понадобится для работы с JSON
 import json
@@ -132,4 +133,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
